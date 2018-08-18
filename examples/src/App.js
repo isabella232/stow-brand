62fdae322332d6c4d96b9d6def95354844d15ed9
MaxBlaushild <blaushmild@gmail.com>
2018-08-18 19:02:33 -0400

    makes all buttons purple

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Logo from './../../src/components/Logo';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  app: {
+    padding: 40
+  },
+  title: {
+    margin: '20px 0px'
+  }
+}
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <Typography variant="title">Logo</Typography>
+      <div className={classes.app}>
+        <Typography variant="title" className={classes.title}>
+          Logo
+        </Typography>
         <Logo style={{ width: 50, height: 50 }}/>
+        <Typography variant="title" className={classes.title}>
+          Button
+        </Typography>
+        <Button>Purple!</Button>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
